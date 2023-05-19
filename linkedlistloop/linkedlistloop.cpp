4ee9d0a60b88b7740a04ca997514e8e67487bfcb
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-19 18:13:16 +0700

    memanggil fungsi traverse untuk membaca informasi yang berada di dalam list

--- linkedlistloop/linkedlistloop.cpp
+++ linkedlistloop/linkedlistloop.cpp
@@ -156,6 +156,14 @@ int main()
 				cout << endl << "data dengan nomor mahasiswa" << nim << " berhasil dihapus" << endl;
 		}
 		break;
+		case '3':
+		{
+			mhs.traverse();
+		}
+		break;
+
 		
+
+
 
 
